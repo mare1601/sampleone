66ef1600bde4afece506e7c9af90003a1b32293c
Mary E. Ellis <mary.ellis@mediahive.com>
2017-05-10 18:26:35 -0400

    New sample site in progress

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+var scrollPos = 0;
+function scroolll() {
+  window.scrollBy(0,900);
+}
+window.addEventListener('scroll',scroolll, false);
+setInterval(scroolll(), 300);
+});

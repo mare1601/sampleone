a6edfd08e983e34cee8c35efc07aa0843ae97ad1
Mary E. Ellis <mary.ellis@mediahive.com>
2017-05-12 17:08:14 -0400

    More files, more styles

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 $(document).ready(function() {
-/*var scrollPos = 0;
-function scroolll() {
-  window.scrollBy(0,900);
-}
-window.addEventListener('scroll',scroolll, false);
-setInterval(scroolll(), 300);*/
-
 	$.scrollify({
 		section:".page",
 		//sectionName:false,
